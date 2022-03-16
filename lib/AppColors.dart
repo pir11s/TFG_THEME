647299b74f8f7b884c3c7fcde37f67eabbfb8a77
pir11s <64949251+pir11s@users.noreply.github.com>
2022-03-16 15:30:46 +0100

    locations switched

---
+++ lib/AppColors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+///App Default Color
+///
+///Class to define the colors of App.
+///
+///19 Colors are defined. Color 11 is undefined.
+///This colors are used to style the colors of whole application.
+class AppColors {
+  // This class is not meant to be instantiated or extended; this constructor
+  // prevents instantiation and extension.
+  // ignore: unused_element
+  AppColors._();
+
+  //Definition of default colors of application
+  static const Color color1 = const Color(0xFF5b1619);
+  static const Color color2 = const Color(0xFF7c1d22);
+  static const Color color3 = const Color(0xFF932226);
+  static const Color color4 = const Color(0xFFa4262c);
+  static const Color color5 = const Color(0xFFae393e);
+  static const Color color6 = const Color(0xFFa19f9d);
+  static const Color color7 = const Color(0xFF605e5c);
+  static const Color color8 = const Color(0xFF3b3a39);
+  static const Color color9 = const Color(0xFF323130);
+  static const Color color10 = const Color(0xFF201f1e);
+  static const Color color12 = const Color(0xFFe91123);
+  static const Color color13 = const Color(0xFFffb902);
+  static const Color color14 = const Color(0xFFfff100);
+  static const Color color15 = const Color(0xFFbad80a);
+  static const Color color16 = const Color(0xFF0f7c10);
+  static const Color color17 = const Color(0xFF02bcf2);
+  static const Color color18 = const Color(0xFF0178d4);
+  static const Color color19 = const Color(0xFF002050);
+  static const Color color20 = const Color(0xFF5c2d91);
+  
+}
